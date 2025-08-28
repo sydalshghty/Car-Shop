32db39d6052e1ad6b77830e8425abac5ce1856f1
Saeed Mohamed <sydalshghty15@gmail.com>
2025-08-28 09:05:14 +0300

    upload-changes

--- src/components/navbar-mobile.jsx
+++ src/components/navbar-mobile.jsx
@@ -1,7 +1,30 @@
+import "../css/Navbar-mobile.css";
 function NavbarMobile () {
+    
     return(
-        <div>
-            <h1>home, navbar-mobile</h1>
+        <div className="navbar-mobile">
+            <div className="container absolute left-0 z-50">
+                <ul>
+                    <li>
+                        <a href="#" className="text-2xl text-white active">الرئيسية</a>
+                    </li>
+                    <li>
+                        <a href="#" className="text-2xl text-white">كيفية الاستخدام ؟</a>
+                    </li>
+                    <li>
+                        <a href="#" className="text-2xl text-white">العروض</a>
+                    </li>
+                    <li>
+                        <a href="#" className="text-2xl text-white">تقديم طلب</a>
+                    </li>
+                    <li>
+                        <a href="#" className="text-2xl text-white">ماذا عنا</a>
+                    </li>
+                    <li>
+                        <a href="#" className="text-2xl text-white">تواصل معنا</a>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
